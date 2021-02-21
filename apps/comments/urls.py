cf297b00f4db1053e71b503c76866ca36785bdbe
sda97g <sda97g@gmail.com>
2021-02-21 22:14:05 +0500

    Add other task fields to the creation response

--- apps/comments/urls.py
+++ apps/comments/urls.py
@@ -2,6 +2,8 @@ from rest_framework.routers import DefaultRouter
 
 from apps.comments.views import CommentsTaskViewSet
 
+app_name = "comments"
+
 router = DefaultRouter()
 router.register("tasks", CommentsTaskViewSet, basename="comments_tasks")
 urlpatterns = router.urls
--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,3 +1,4 @@
+from rest_framework.fields import ReadOnlyField
 from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ModelViewSet
 
@@ -5,22 +6,17 @@ from apps.comments import models
 
 
 class CommentsTaskSerializer(ModelSerializer):
+    created_at = ReadOnlyField()
+    fetched_at = ReadOnlyField()
+    finished_at = ReadOnlyField()
+    status = ReadOnlyField()
+    status_message = ReadOnlyField()
+
     class Meta:
         model = models.CommentsTask
         fields = "__all__"
 
 
-class CommentsTaskPostSerializer(ModelSerializer):
-    class Meta:
-        model = models.CommentsTask
-        fields = ["url"]
-
-
 class CommentsTaskViewSet(ModelViewSet):
     queryset = models.CommentsTask.objects.all()
     serializer_class = CommentsTaskSerializer
-
-    def get_serializer_class(self):
-        if self.request.method == "POST":
-            return CommentsTaskPostSerializer
-        return self.serializer_class
