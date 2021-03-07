21e0b3a5a0fad1ec5e8b4b3d685a6272336fbdcd
sda97g <sda97g@gmail.com>
2021-03-07 20:46:14 +0500

    Add comments actions: of_post, top_comments_of_post, top_branch

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,8 +1,12 @@
+from rest_framework.decorators import action
 from rest_framework.fields import ReadOnlyField
+from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import NotFound
 
 from apps.comments import models
+from apps.posts.models import Post
 
 
 class CommentsTaskSerializer(ModelSerializer):
@@ -31,3 +35,51 @@ class CommentSerializer(ModelSerializer):
 class CommentViewSet(ModelViewSet):
     queryset = models.Comment.objects.all()
     serializer_class = CommentSerializer
+
+    @action(detail=False, url_path="of-post/(?P<post_pk>[0-9]+)")
+    def of_post(self, request, post_pk):
+        try:
+            post = Post.objects.get(pk=post_pk)
+        except Post.DoesNotExist:
+            raise NotFound(detail="Post not found.")
+
+        queryset = post.comments.all()
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, url_path="of-post/(?P<post_pk>[0-9]+)/top")
+    def top_comments_of_post(self, request, post_pk):
+        try:
+            post = Post.objects.get(pk=post_pk)
+        except Post.DoesNotExist:
+            raise NotFound(detail="Post not found.")
+
+        queryset = post.comments.filter(parent_comment=None)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, url_path="top-branch")
+    def top_branch(self, request, pk):
+        comment = models.Comment.objects.get(pk=pk)
+
+        branch = []
+        while True:
+            comment = comment.children_comments.order_by("-rating").first()
+            if comment is None:
+                break
+            branch.append(comment)
+
+        serializer = self.get_serializer(branch, many=True)
+        return Response(serializer.data)
