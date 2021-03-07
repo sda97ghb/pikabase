ddb5bf7d87099a57b24cb7db175fd7202d6be2eb
sda97g <sda97g@gmail.com>
2021-03-07 20:45:27 +0500

    Implement process_comments task

--- apps/comments/tasks.py
+++ apps/comments/tasks.py
@@ -2,7 +2,10 @@ import logging
 
 from dramatiq import actor
 
-from apps.comments.models import CommentsTask, StatusChoices
+from apps.comments.models import CommentsTask, StatusChoices, Comment
+from apps.pikabu_users.models import PikabuUser
+from apps.posts.models import Post
+from pikabu.api.comments import story_comments
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -18,6 +21,45 @@ def process_comments(comments_task_pk):
 
     log.info("Processing %r", comments_task)
 
-    comments_task.status = StatusChoices.ERROR.value
-    comments_task.status_message = "Processing task is not implemented"
+    comments_task.status = StatusChoices.PROCESSING.value
+    comments_task.status_message = "Fetching comments"
+    comments_task.save()
+
+    post, created = Post.objects.get_or_create(url=comments_task.url)
+    if created:
+        log.info("Created post %s", post.url)
+
+    affected_comments = []
+    for comment in story_comments(story_url=comments_task.url):
+        pikabu_user, created = PikabuUser.objects.get_or_create(
+            username=comment.username
+        )
+        if created:
+            log.info("Created user %s", pikabu_user.username)
+
+        comment, created = Comment.objects.update_or_create(
+            pikabu_id=comment.id,
+            defaults={
+                "pikabu_parent_id":comment.parent_id,
+                "user":pikabu_user,
+                "text":comment.text,
+                "date":comment.date,
+                "rating":comment.rating,
+                "post":post,
+            }
+        )
+        affected_comments.append(comment)
+
+    comments_task.status_message = "Setting up parent comments"
+    comments_task.save()
+
+    for comment in affected_comments:
+        if comment.pikabu_parent_id is not None and comment.parent_comment is None:
+            comment.parent_comment = Comment.objects.get(
+                pikabu_id=comment.pikabu_parent_id
+            )
+            comment.save()
+
+    comments_task.status = StatusChoices.COMPLETED.value
+    comments_task.status_message = "Success"
     comments_task.save()
